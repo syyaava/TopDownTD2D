02d1b929d103c262d6062f2a8e6ac6dec747cf64
600IQ <aspnetforheroku@mail.ru>
2023-03-23 13:21:43 +0500

    Add camera movement bounds.

--- Assets/Player/Camera/CameraMovement.cs
+++ Assets/Player/Camera/CameraMovement.cs
@@ -1,39 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraMovement : MonoBehaviour
 {
     public float PanSpeed = 30f;
     public float PanBoardThickness = 10f;
     public float ScrollSpeed = 1f;
+    public Tilemap PlayableField;
+    public float BoundsOffsetMultiplier = 2f;
 
     private bool doMovement => !GamePauseController.IsPause;
     private float minScrollSize = 2.5f;
     private float maxScrollSize = 10f;
+    private Vector2 cameraMovement;
+    private Bounds movementBounds;
     // Update is called once per frame
+
+    private void Start()
+    {
+        movementBounds = PlayableField.localBounds;
+        movementBounds.min -= (Vector3)(Vector2.one * BoundsOffsetMultiplier);
+        movementBounds.max += (Vector3)(Vector2.one * BoundsOffsetMultiplier);
+    }
+
     void Update()
     {
         if (!doMovement)
             return;
 
+        Movement();
+
+        ChangeCameraSize();
+    }
+
+    private void Movement()
+    {
+        cameraMovement = Vector2.zero;
         if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - PanBoardThickness)
         {
-            transform.Translate(Vector2.up * PanSpeed * Time.deltaTime, Space.World);
+            cameraMovement += Vector2.up * PanSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S) || Input.mousePosition.y <= PanBoardThickness)
         {
-            transform.Translate(Vector2.down * PanSpeed * Time.deltaTime, Space.World);
+            cameraMovement += Vector2.down * PanSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - PanBoardThickness)
         {
-            transform.Translate(Vector2.right * PanSpeed * Time.deltaTime, Space.World);
+            cameraMovement += Vector2.right * PanSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= PanBoardThickness)
         {
-            transform.Translate(Vector2.left * PanSpeed * Time.deltaTime, Space.World);
-        }
+            cameraMovement += Vector2.left * PanSpeed * Time.deltaTime;
+        }        
 
+        transform.Translate(cameraMovement, Space.World);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, movementBounds.min.x, movementBounds.max.x), 
+            Mathf.Clamp(transform.position.y, movementBounds.min.y, movementBounds.max.y), transform.position.z);
+    }
+
+    private void ChangeCameraSize()
+    {
         var scroll = Input.GetAxis("Mouse ScrollWheel");
         Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - scroll * ScrollSpeed * Time.deltaTime, minScrollSize, maxScrollSize);
     }
